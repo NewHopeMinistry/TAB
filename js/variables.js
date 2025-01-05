20fe90300723efb405debcc61595810b990ef70f
NewHopeMinistry <gbutler27370@live.com>
2025-01-05 15:42:51 -0500

    Update TWF

--- js/variables.js
+++ js/variables.js
@@ -1,4 +1,4 @@
-var dateEdited = '1-4-2025'
+var dateEdited = '1-5-2025'
 if (document.getElementById('id-lastEdited')) { document.getElementById('id-lastEdited').textContent = `Last Edited: ${dateEdited}`; }
 
 var boxesOpen = false;
